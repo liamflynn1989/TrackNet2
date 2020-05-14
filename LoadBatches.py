4219e207d72372ad3cef3c8414e6ac7e53ea39f2
liamflynn1989 <liamflynn1989@gmail.com>
2020-05-14 14:46:44 +1000

    first commit

---
+++ LoadBatches.py
@@ -0,0 +1,115 @@
+import numpy as np
+import cv2
+import itertools
+import csv
+from collections import defaultdict
+import os
+
+direct = 'C:/Users/liamf/Google Drive/ML/TrackNet/Code_Python3/'
+#get input array
+def getInputArr( path ,path1 ,path2 , width , height):
+    
+	try:
+		#read the image
+		img = cv2.imread(os.path.join(direct,path), 1)
+		#resize it 
+		img = cv2.resize(img, ( width , height ))
+		#input must be float type
+		img = img.astype(np.float32)
+
+		#read the image
+		img1 = cv2.imread(os.path.join(direct,path1), 1)
+		#resize it 
+		img1 = cv2.resize(img1, ( width , height ))
+		#input must be float type
+		img1 = img1.astype(np.float32)
+
+		#read the image
+		img2 = cv2.imread(os.path.join(direct,path2), 1)
+		#resize it 
+		img2 = cv2.resize(img2, ( width , height ))
+		#input must be float type
+		img2 = img2.astype(np.float32)
+
+		#combine three imgs to  (width , height, rgb*3)
+		imgs =  np.concatenate((img, img1, img2),axis=2)
+
+		#since the odering of TrackNet  is 'channels_first', so we need to change the axis
+		imgs = np.rollaxis(imgs, 2, 0)
+		return imgs
+
+	except Exception as e:
+
+		print(path , e)
+
+
+
+#get output array
+def getOutputArr( path , nClasses ,  width , height  ):
+
+	seg_labels = np.zeros((  height , width  , nClasses ))
+	try:
+		img = cv2.imread(os.path.join(direct,path), 1)
+		img = cv2.resize(img, ( width , height ))
+		img = img[:, : , 0]
+
+		for c in range(nClasses):
+			seg_labels[: , : , c ] = (img == c ).astype(int)
+
+	except Exception as e:
+		print(e)
+		
+	seg_labels = np.reshape(seg_labels, ( width*height , nClasses ))
+	return seg_labels
+
+
+
+#read input data and output data
+def InputOutputGenerator( images_path,  batch_size,  n_classes , input_height , input_width , output_height , output_width ):
+
+
+	#read csv file to 'zipped'
+	columns = defaultdict(list)
+	with open(images_path) as f:
+	    reader = csv.reader(f)
+	    next(reader)
+	    for row in reader:
+	        for (i,v) in enumerate(row):
+	            columns[i].append(v)
+	zipped = itertools.cycle( zip(columns[0], columns[1], columns[2], columns[3]) )
+
+	while True:
+		Input = []
+		Output = []
+		#read input&output for each batch
+		for _ in range( batch_size) :
+			path, path1, path2 , anno = next(zipped)
+			Input.append( getInputArr(path, path1, path2 , input_width , input_height) )
+			Output.append( getOutputArr( anno , n_classes , output_width , output_height) )
+		#return input&output
+		yield np.array(Input) , np.array(Output)
+
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(10,5))
+ax = fig.add_subplot(111)
+
+from pathlib import Path
+
+
+path = 'Dataset/Dataset/game4/Clip7/groundtruth/Clip7/0177.png'
+
+#path = 'C:/Users/liamf/Google Drive/ML/TrackNet/Code_Python3/Dataset/Dataset/game4/Clip7/0017.jpg'
+img = cv2.imread(os.path.join(direct,path))
+plt.imshow(img)
+
+
+path1 = 'Dataset/Dataset/game4/Clip7/0016.jpg'
+path2 = 'Dataset/Dataset/game4/Clip7/0015.jpg'
+width = 360
+height = 640
+nClasses = 256
+
+#getInputArr( path ,path1 ,path2 , width , height)
+getOutputArr( path , nClasses ,  width , height  )
